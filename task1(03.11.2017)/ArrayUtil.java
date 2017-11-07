767e20a0a6df92d01dde2f2580319078cf5e92d3
Viktor <borlandfcsd@gmail.com>
2017-11-07 02:20:31 +0200

    task0(3\4), task1(4\6)

--- task1(03.11.2017)/ArrayUtil.java
+++ task1(03.11.2017)/ArrayUtil.java
@@ -1,27 +1,27 @@
 package task1;
 
-public class UtilArray {
-    protected static void addContent(int[] arr) {
+public class ArrayUtil {
+    public static void fillArray(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
-            arr[i] = (int)(Math.random()*100) - 50;
+            arr[i] = (int) (Math.random() * 100) - 50;
         }
     }
 
-    protected static void addContent(int[][] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            for(int j = 0; j < arr[i].length; j++) {
-                arr[i][j] = (int) (Math.random() * 10);
+    public static void fillMatrix(int[][] matrix, int maxNumer) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                matrix[i][j] = (int) (Math.random() * maxNumer);
             }
         }
     }
 
     public static void copyArray(float[] arr, float[] arrCp) {
-        for(int i = 0; i< arr.length; i++){
+        for (int i = 0; i < arr.length; i++) {
             arrCp[i] = arr[i];
         }
     }
 
-    public static void showMatrix(int[][] arr) {
+    public static void printMatrix(int[][] arr) {
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
                 System.out.print("[" + arr[i][j] + "]");
@@ -30,18 +30,17 @@ public class UtilArray {
         }
     }
 
-    protected  static  void swap (int[] arr, int elem1, int elem2) {
+    public static void swap(int[] arr, int elem1, int elem2) {
         int swap = arr[elem1];
         arr[elem1] = arr[elem2];
         arr[elem2] = swap;
     }
 
-    protected static  void swap (float[] arr, int elem1, int elem2) {
+    public static void swap(float[] arr, int elem1, int elem2) {
         float swap = arr[elem1];
         arr[elem1] = arr[elem2];
         arr[elem2] = swap;
     }
-
 
 
 }
