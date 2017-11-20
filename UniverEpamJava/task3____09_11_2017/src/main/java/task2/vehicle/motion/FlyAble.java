cbf36f532f6a7b0420a935f282294ff864a32234
Viktor <borlandfcsd@gmail.com>
2017-11-20 19:43:38 +0200

    task4

--- UniverEpamJava/task3____09_11_2017/src/main/java/task2/vehicle/motion/FlyAble.java
+++
@@ -1,6 +0,0 @@
-package UniverEpamJava.task2____07_11_2017.vehicle.motion;
-
-/**Интерфейс отвечающий за перемещение по воздуху*/
-public interface FlyAble {
-    public void fly();
-}
--- UniverEpamJava/task3____09_11_2017/src/main/java/task2/vehicle/motion/SwimAble.java
+++
@@ -1,6 +0,0 @@
-package UniverEpamJava.task2____07_11_2017.vehicle.motion;
-
-/**Интерфейс отвечающий за перемещение по воде*/
-public interface SwimAble {
-    public void swim();
-}
