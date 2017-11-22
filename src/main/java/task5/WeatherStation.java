e95d4778daf4debfa1e623fbcf5e8c452cc09ede
Viktor <borlandfcsd@gmail.com>
2017-11-22 16:55:39 +0200

    remove .idea

--- src/main/java/task5/WeatherStation.java
+++ src/main/java/task5/WeatherStation.java
@@ -3,17 +3,14 @@ package task5;
 public class WeatherStation {
 
     public static void main(String[] args) {
-    /*    WeatherData weatherData = new WeatherData();
+        WeatherData weatherData = new WeatherData();
         CurrentConditionalDisplay currentDisplay = new CurrentConditionalDisplay(weatherData);
         StatisticsDisplay statisticsDisplay = new StatisticsDisplay(weatherData);
         ForecastDisplay forecastDisplay = new ForecastDisplay(weatherData);
         HeatIndexDisplay heatIndexDisplay = new HeatIndexDisplay(weatherData);
 
         weatherData.setMeasurements(20,70,30.4f);
-        weatherData.setMeasurements(23,70,30.4f);*/
-        int a = 8;
-         a += a++ + ++a + a++;
-        System.out.print(a);
+        weatherData.setMeasurements(23,70,30.4f);
     }
 
 }
